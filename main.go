067d94d038234636f3f336ae5050b1dedd20babd
Han Zhao <hanzhao>
2020-05-20 13:02:17 -0400

    disabled reply on EVENTS_AND_REPORTS_RESPONE

--- main.go
+++ main.go
@@ -314,14 +314,14 @@ func handleRequest(conn net.Conn, tags llrp.Tags) {
 			log.Info("<<< SET_READER_CONFIG_RESPONSE")
 		} else if header == llrp.EventsAndReportsHeader {
 			log.Info(">>> EVENTS_AND_REPORTS")
-			var data []byte
-			roar := llrp.NewROAccessReport(data, currentMessageID)
-			err := roar.Send(conn)
-			atomic.AddUint32(&currentMessageID, 1)
-			runtime.Gosched()
-			if err != nil {
-				log.Warn(err)
-			}
+			// var data []byte
+			// roar := llrp.NewROAccessReport(data, currentMessageID)
+			// err := roar.Send(conn)
+			// atomic.AddUint32(&currentMessageID, 1)
+			// runtime.Gosched()
+			// if err != nil {
+			// 	log.Warn(err)
+			// }
 			log.Info("<<< EVENTS_AND_REPORTS_RESPONSE")
 		} else if header == llrp.DeleteAccessSpecHeader {
 			log.Info(">>> DELETE_ACCESS_SPEC")
